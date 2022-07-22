4d5e94d23c107dcb25a5f13d9b40a37900323cd5
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-07-22 16:55:24 +0200

    add controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,3 @@
+export * from './ping.controller';
+export * from './cars.controller';
+export * from './user.controller';
---
+++ src/models/car.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Car extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  model: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  year: string;
+
+
+  constructor(data?: Partial<Car>) {
+    super(data);
+  }
+}
+
+export interface CarRelations {
+  // describe navigational properties here
+}
+
+export type CarWithRelations = Car & CarRelations;
